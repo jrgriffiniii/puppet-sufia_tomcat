8e543bc33cad0c0ec969834aa59b518870b4f843
jrgriffiniii <griffinj@lafayette.edu>
2015-02-06 11:05:30 -0500

    HYDRA-3 #time 20m Resolving a syntax error within the requirement for "rails_bundle_install"

--- manifests/install.pp
+++ manifests/install.pp
@@ -61,7 +61,7 @@ class sufia_tomcat::install inherits sufia_tomcat {
     # command => '/usr/local/rvm/gems/ruby-2.1.5@global/bin/bundle install',
     command => '/usr/bin/env sudo -i su -c "cd /var/www/sufia && bundle update && bundler install"',
     cwd => '/var/www/sufia',
-    require => Package['git'], File['/var/www/sufia/Gemfile']
+    require => [ Package['git'], File['/var/www/sufia/Gemfile'] ]
   }
 
   # Generate the Sufia instance
